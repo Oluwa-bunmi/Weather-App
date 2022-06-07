4dcd497b95f2a98d1a14f445f52f57ef909a6f6c
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-07 03:19:41 +0100

    success

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let weather = {
   "apiKey": "8d4ce9534d0890f4fe7f8075f5257aa3",
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Nigeria&appid=8d4ce9534d0890f4fe7f8075f5257aa3"
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
   },
+  displayWeather: function(data){
+
+  }
 };
